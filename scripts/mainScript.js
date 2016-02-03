682deff32f8e34fdccf1c104f8688675fc32f002
tuchung <tuthienchung@gmail.com>
2016-02-03 00:14:34 -0500

    add angular function to page

---
+++ scripts/mainScript.js
@@ -0,0 +1,23 @@
+var mainApp = angular.module('mainApp', ['ngRoute']);
+
+mainApp.config(function($routeProvider){
+  $routeProvider
+  .when('/', {
+    templateUrl : 'pages/home.html',
+    controller : 'mainController'
+  })
+
+  .when('/about',{
+    templateUrl : 'pages/contactMe.html',
+    controller : 'contactController'
+  });
+});
+
+mainApp.controller('mainController', function($scope){
+
+    $scope.message = "This Website provide service for user to file their tax.";
+});
+
+mainApp.controller('aboutController', function($scope){
+  $scope.message = "My name is Charles Pier and I am a accountant professor";
+});
